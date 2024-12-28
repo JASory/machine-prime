153638b5e2a03b803c9ea0c35533e8eaaf7477f1
JASory <jasory@rust-cas.org>
2024-12-28 09:39:39 -0700

    Update primality.bat
    Update for new feature names

--- primality.bat
+++ primality.bat
@@ -1,13 +1,13 @@
 @echo off
  mkdir libprime
- echo Select 1  for Default, 2 for Small, 3  for Tiny, 4 for SSMR
+ echo Select 1  for Default, 2 for Lucas, 3  for Table, 4 for SSMR 
  set /p flag=""
  if %flag%==1 (rustc -C opt-level="3" src/lib.rs -o libprime/prime.dll --crate-type cdylib --target x86_64-pc-windows-msvc
  echo Built the Default variant)
- if %flag%==2 (rustc -C opt-level="3" src/lib.rs -o libprime/prime.dll --crate-type cdylib --target x86_64-pc-windows-msvc --cfg feature=\"small\"
- echo Built the Small variant) 
- if %flag%==3 (rustc -C opt-level="3" src/lib.rs -o libprime/prime.dll --crate-type cdylib --target x86_64-pc-windows-msvc --cfg feature=\"tiny\"
- echo Built the Tiny variant)
+ if %flag%==2 (rustc -C opt-level="3" src/lib.rs -o libprime/prime.dll --crate-type cdylib --target x86_64-pc-windows-msvc --cfg feature=\"lucas\"
+ echo Built the Lucas variant) 
+ if %flag%==3 (rustc -C opt-level="3" src/lib.rs -o libprime/prime.dll --crate-type cdylib --target x86_64-pc-windows-msvc --cfg feature=\"table\"
+ echo Built the Table variant)
  if %flag%==3 (rustc -C opt-level="3" src/lib.rs -o libprime/prime.dll --crate-type cdylib --target x86_64-pc-windows-msvc --cfg feature=\"ssmr\"
  echo Built the SSMR variant) 
 pause
